8b3bb1a0ee74a697a95d9580a5424300ca3d6890
George Boahene <olibo1zeemoni13@gmail.com>
2023-08-22 09:09:26 +0000

    final commit

---
+++ AnimatingNumberChanges/AnimatableNumberModifier.swift
@@ -0,0 +1,44 @@
+//
+//  AnimatableNumberModifier.swift
+//  AnimatingNumberChanges
+//
+//  Created by Olibo moni on 22/08/2023.
+//
+
+import SwiftUI
+
+struct AnimatableNumberModifier: AnimatableModifier {
+    var number: Int
+    //Usually a double, Int conforms to VectorArithmetic
+    
+    var animatableData: Int {
+        get { number }
+        set { number = newValue }
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                Text("\(Int(number))")
+            )
+    }
+}
+
+extension View {
+    func animatingOverlay(for number: Int) -> some View {
+        modifier(AnimatableNumberModifier(number: number))
+    }
+}
+
+
+extension Int: VectorArithmetic {
+    public mutating func scale(by rhs: Double) {
+        self = Int(Double(self) * rhs )
+    }
+    
+    public var magnitudeSquared: Double {
+        Double(self * self)
+    }
+    
+    
+}
