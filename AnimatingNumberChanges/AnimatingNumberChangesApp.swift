7e31b311fc1f692ece27855bde5191d47bc3bf6b
George Boahene <olibo1zeemoni13@gmail.com>
2023-08-22 06:44:54 +0000

    first commit

---
+++ AnimatingNumberChanges/AnimatingNumberChangesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatingNumberChangesApp.swift
+//  AnimatingNumberChanges
+//
+//  Created by Olibo moni on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingNumberChangesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
